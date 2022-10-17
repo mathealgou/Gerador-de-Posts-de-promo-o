dc90d1925f09c9feb239a09b842c68f73afafa34
Matheus <matheus.goulart@penseavanti.com.br>
2022-10-17 12:01:12 -0300

    fix [Terminal]: Now showing only 5 promos at a time and with prettier colors.

--- terminal/__init__.py
+++ terminal/__init__.py
@@ -1,6 +1,6 @@
 class Terminal:
     def choose_promo(promos: list):
-        x =[i["name"] for i in promos]
+        x =[i["name"] for i in promos][:5]
         for i, j in enumerate(x):
-            print(i+1, j)
+            print(f"\u001b[32m{i+1}  -", "\u001b[37m",j)
         return promos[int(input("Choose a promo: ")) - 1]
